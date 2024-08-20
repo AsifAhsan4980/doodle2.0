410b27876f44ea8d6c00c6fa823d49442064452a
Meehedi-hasan <mehedi@thedoodleinc.com>
2024-08-20 18:00:57 +0600

    done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,13 @@
 import "@/styles/globals.css";
-import { Poppins } from '@next/font/google';
-const poppins = Poppins({
-  subsets: ['latin'], // Define the subsets you need
-  weight: ['400', '500', '600', '700'], // Define the weights you need
-});
+// import { Poppins } from '@next/font/google';
+// const poppins = Poppins({
+//   subsets: ['latin'], // Define the subsets you need
+//   weight: ['400', '500', '600', '700'], // Define the weights you need
+// });
 
 export default function App({ Component, pageProps }) {
   return (
-      <div className={poppins.className}>
+      <div >
         <Component {...pageProps} />;
       </div>
   )
