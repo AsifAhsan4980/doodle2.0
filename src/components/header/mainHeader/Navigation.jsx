d0cfd7646e0e7b5bf6f86cd6231c053e0055f065
Meehedi-hasan <mehedi@thedoodleinc.com>
2024-08-20 18:53:36 +0600

    done

--- src/components/header/mainHeader/Navigation.jsx
+++ src/components/header/mainHeader/Navigation.jsx
@@ -1,28 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { NavLink } from 'react-router-dom';
 import Link from "next/link"
+import DownArrow from '@/components/Icon/DownArrow';
 
 function Navigation() {
   const navItems = [
-    { label: 'Home',link:'/', isActive: true },
-    { label: 'Services',link:'/services' },
-    { label: 'Industries & Expertise',link:'/', hasDropdown: true },
-    { label: 'Case Studies',link:'/' },
-    { label: 'Company',link:'/', hasDropdown: true }
+    { id: '1', label: 'Home', link: '/', isActive: true },
+    { id: '2', label: 'Services', link: '/services' },
+    { id: '3', label: 'Industries & Expertise', link: '/', hasDropdown: true },
+    { id: '4', label: 'Case Studies', link: '/' },
+    { id: '5', label: 'Company', link: '/', hasDropdown: true }
   ];
+  const [arrowMoodChange, setArrowMoodChange] = useState(false)
+
 
   return (
     <nav className="flex justify-evenly items-center text-base font-semibold pt-5 text-zinc-800">
       {navItems.map((item, index) => (
-        <Link key={index} href={item.link} className={`flex gap-1.5 ${item.isActive ? 'font-bold text-orange-500' : ''}`}>
-          <div className="grow">{item.label}</div>
+        <Link key={index} href={item.link} className={`flex justify-start items-center gap-1 ${item.isActive ? 'font-bold text-orange-500' : ''}`}>{item.label}
           {item.hasDropdown && (
-            <img 
-              loading="lazy" 
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/3e4edb1eae8c9114383c96830ed432193df4fe98ac1877c36938ab1e1dadd501?placeholderIfAbsent=true&apiKey=b0c497e9dfc54574b68d5cdde728b1cb" 
-              className="object-contain shrink-0 my-auto aspect-square w-[15px]" 
-              alt=""
-            />
+            <DownArrow onClick={() => {setArrowMoodChange(!arrowMoodChange); }} className={`transform transition-transform duration-300 ease-in-out ${arrowMoodChange ? 'rotate-180' : ''}`} />
           )}
         </Link>
       ))}
